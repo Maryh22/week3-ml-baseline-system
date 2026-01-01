2b691f2e71a6605a4c38d378ceb3457f936c43b4
Maryh22 <maryhfhdalhajwj@gmail.com>
2026-01-01 16:06:29 +0300

    day4: predict cli + skew check

--- tests/test_train_predict.py
+++ tests/test_train_predict.py
@@ -5,7 +5,8 @@ from ml_baseline.sample_data import make_sample_feature_table
 from ml_baseline.train import run_train
 from ml_baseline.predict import run_predict, resolve_run_dir
 
-
+"""
+"""
 def test_train_and_predict(tmp_path: Path) -> None:
     # Create a temp repo-like root
     root = tmp_path
